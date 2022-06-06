f0afac6e1ecc9bacc2a0ef40aa5616457f4a619e
Edward Han <edwrdhn@gmail.com>
2022-06-06 10:19:32 -0400

    moved google api to script, no config file

--- src/components/NewFindBusiness.js
+++ src/components/NewFindBusiness.js
@@ -5,19 +5,11 @@ import Autocomplete from "react-google-autocomplete";
 
 const GoogleAutoFill = ({setGoogleInfo}) => {
 
-    const [value, setValue] = useState(null);
-    const handleChange = (val) => {
-        console.log(val)
-        setGoogleInfo(val)
-    
-
-    }
   
     return (
       <div>
     <Autocomplete
         style={{ width: "250px", padding: "20px 0" }}
-        apiKey={process.env.REACT_APP_GOOGLE_API_STRING}
         onPlaceSelected={(place) => {
              console.log(place);
              setGoogleInfo(place)
