6eaf547e8e19f18a9b6bac536d13d86809addbf4
Edward Han <edwrdhn@gmail.com>
2022-06-05 23:12:10 -0400

    added environmentplugin

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 module.exports = function override(config, env) {
     // New config, e.g. config.plugins.push...
+    config.plugins.push(new webpack.EnvironmentPlugin(['REACT_APP_GOOGLE_API_STRING']))
     return config
 }
