749e77f7737755e7a9b449e11c164c8afd680a7b
Edward Han <edwrdhn@gmail.com>
2022-06-05 23:16:42 -0400

    added require webpack

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,7 @@
+
+
+var webpack = require('webpack');
+
 module.exports = function override(config, env) {
     // New config, e.g. config.plugins.push...
     config.plugins.push(new webpack.EnvironmentPlugin(['REACT_APP_GOOGLE_API_STRING']))
